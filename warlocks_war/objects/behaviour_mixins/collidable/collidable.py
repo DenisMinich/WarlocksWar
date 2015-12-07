4b9263f9c7571ecf504610f919244e410a63d360
Denis Minich <denis.minich.fitr@gmail.com>
2015-12-07 18:38:55 +0300

    Moved out collission logic. Added collission event.

--- warlocks_war/objects/behaviour_mixins/collidable/collidable.py
+++ warlocks_war/objects/behaviour_mixins/collidable/collidable.py
@@ -11,56 +11,22 @@ class Collidable(WorldObject):
     def __init__(self, *args, **kwargs):
         super(Collidable, self).__init__(*args, **kwargs)
         self.add_to_collections(["collidable"])
+        self.register_event_type('on_collide')
         self.bind(on_update=self.process_collisions)
 
     def get_resistance_vector(self, widget):
         intersection = Collidable.get_intersection(self, widget)
-        print('intersection', intersection)
         affection_zone = self._get_affection_zone(intersection)
-        print('affection_zone', affection_zone)
         result = self._calculate_resistance_vector(affection_zone)
-        print('resistance vector', result)
         return result
 
     def process_collisions(self, instance):
         for widget in Collector.get_collection("collidable"):
             if self is not widget and self.collide_widget(widget) and widget.collide_widget(self):
-                base, additional = (widget, self) if self.static else (self, widget)
-                base.velocity, additional.velocity = Collidable.get_velocity_after_collission(
-                    base, additional)
-                while self.collide_widget(widget) and widget.collide_widget(self):
-                    if not self.static:
-                        self.move(self)
-                    if not widget.static:
-                        widget.move(widget)
+                self.dispatch("on_collide", widget)
 
-    @staticmethod
-    def get_velocity_after_collission(first, second):
-        collission_vector_x = second.get_resistance_vector(first)
-        collission_vector_y = collission_vector_x.rotate(90)
-        v1, v2 = first.velocity, second.velocity
-        system_speed = Vector(v2)
-        v1, v2 = Vector(v1) - system_speed, Vector(v2) - system_speed
-        system_rotate_angle = Vector(1, 0).angle(collission_vector_x)
-        v1a, v1b = Vector(v1).rotate(system_rotate_angle)
-        mass_ratio = 0 if not second.mass else first.mass / second.mass
-        u1a_1, u1a_2 = Collidable.solve_quadratic_equation(
-            a = mass_ratio + 1,
-            b = -2 * mass_ratio * v1a,
-            c = (mass_ratio - 1) * v1a ** 2)
-        u1a = u1a_1 if u1a_1 != v1a else u1a_2
-        u1b = v1b
-        u2a = mass_ratio * (v1a - u1a)
-        u2b = 0
-        u1 = Vector(u1a, u1b).rotate(-system_rotate_angle)
-        u2 = Vector(u2a, u2b).rotate(-system_rotate_angle)
-        u1, u2 = u1 + system_speed, u2 + system_speed
-        return u1, u2
-
-    @staticmethod
-    def solve_quadratic_equation(a, b, c):
-        D = b ** 2 - 4 * a * c
-        return (-b + D ** .5) / (2 * a), (-b - D ** .5) / (2 * a)
+    def on_collide(self, widget):
+        pass
 
     @staticmethod
     def get_intersection(first, second):
--- warlocks_war/objects/entities/actor/actor.py
+++ warlocks_war/objects/entities/actor/actor.py
@@ -1,7 +1,11 @@
+from warlocks_war.objects.behaviour_mixins.collidable.collission_processors import (
+    ElasticCollissionProcessor)
 from warlocks_war.objects.behaviour_mixins import (
     Movable, ImageView, Collidable)
 from warlocks_war.objects.world_object import WorldObject
 
 
 class Actor(Movable, Collidable, ImageView, WorldObject):
-    pass
+    def __init__(self, *args, **kwargs):
+        super(Actor, self).__init__(*args, **kwargs)
+        self.bind(on_collide=ElasticCollissionProcessor.process_collission)
