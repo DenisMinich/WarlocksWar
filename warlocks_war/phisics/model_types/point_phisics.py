91148027c6095df8b40ad5b62a6c70042afd35f7
Denis Minich <denis.minich.fitr@gmail.com>
2015-12-16 10:22:00 +0300

    Updated point phisics model

--- warlocks_war/phisics/model_types/point_phisics.py
+++ warlocks_war/phisics/model_types/point_phisics.py
@@ -5,16 +5,15 @@ from warlocks_war.phisics.model_types.base_phisics import BasePhisics
 
 class PointPhisics(BasePhisics):
 
-    def __init__(self, *args, gravity=0, coords=(0, 0), affection_radius=0, **kwargs):
+    def __init__(self, *args, gravity=0, affect_radius=0, **kwargs):
         super(PointPhisics, self). __init__(*args, **kwargs)
         self.gravity = gravity
-        self.coords = Vector(coords)
-        self.affection_radius = affection_radius
+        self.affect_radius = affect_radius
 
     def _get_acceleration(self, world_object):
-        acceleration_vector = Vector(self.coords.x - world_object.x, self.coords.y - world_object.y)
-        if self.affection_radius < acceleration_vector.length():
+        acceleration_vector = Vector(self.x - world_object.x, self.y - world_object.y)
+        if self.affect_radius < acceleration_vector.length():
             acceleration_vector *= 0
         else:
-            acceleration_vector *= (1 - acceleration_vector.length() / self.affection_radius) * self.gravity
+            acceleration_vector *= (1 - acceleration_vector.length() / self.affect_radius) * self.gravity
         return acceleration_vector
