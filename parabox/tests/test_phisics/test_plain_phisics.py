8d4612a407b80913cd005ea88763273504bf4ebd
Denis Minich <denis.minich.fitr@gmail.com>
2015-12-22 17:28:45 +0300

    Added phisics tests

---
+++ parabox/tests/test_phisics/test_plain_phisics.py
@@ -0,0 +1,22 @@
+import unittest
+from kivy.vector import Vector
+
+from parabox.behaviour import Movable
+from parabox.phisics import PlainPhisics
+
+
+class TestCollector(unittest.TestCase):
+    def setUp(self):
+        self.affected = Movable()
+        self.down = PlainPhisics(
+            gravity=(0, -1), affect_objects=[self.affected])
+        self.right = PlainPhisics(
+            gravity=(0, 2), affect_objects=[self.affected], angle=-90)
+
+    def test_acceleration(self):
+        self.assertEqual(self.affected.acceleration, Vector(0, 0))
+        self.down.update()
+        self.right.update()
+        self.assertAlmostEqual(self.affected.acceleration[0], 2)
+        self.assertAlmostEqual(self.affected.acceleration[1], -1)
+
